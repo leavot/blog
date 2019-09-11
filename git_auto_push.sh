96e0dba090f085727f8c676d69e6319abad186a7
Ren Chuanlei <renchuanlei@kuaidaoapp.com>
2019-09-11 15:52:10 +0800

    auto push office

--- git_auto_push.sh
+++ git_auto_push.sh
@@ -1,5 +1,9 @@
 #!/bin/bash 
 
+# add following to crontab to enable auto git push for this repo
+# */30 * * * * /media/sf_github/leavot/blog/git_auto_push.sh >/dev/null 2>&1
+
+#get current repo's path
 repo_path=$(cd $(dirname $0); pwd)
 echo $repo_path
 cd $repo_path && ls && git pull && git pull && git add . && git commit -m "auto push office" && git push origin master
